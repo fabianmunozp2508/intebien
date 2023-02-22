e920530e2482a0035c61289732a6b5452da6888d
fpuello <fpuello@opensistemas.com>
2023-02-22 10:42:11 -0500

    cambios

--- src/app/pages/child-routes.module.ts
+++ src/app/pages/child-routes.module.ts
@@ -3,16 +3,20 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '../components/home/home/home.component';
 import { AboutComponent } from '../components/home/home/about/about.component';
+import { AddressComponent } from '../components/home/home/address/address.component';
+import { CoursesComponent } from '../components/home/home/courses/courses.component';
+import { ReviewComponent } from '../components/home/home/review/review.component';
+import { FeatureComponent } from '../components/home/home/feature/feature.component';
 
 const childRoutes: Routes = [
 
   { path: '', component: HomeComponent, data: { titulo: 'Inicio' } },
-  { path: 'nosotros', component: AboutComponent, data: { titulo: 'Nosotros' } },
- 
-  // { path: 'programas', component: ProgramasComponent, data: { titulo: 'Programas' } },
+  { path: 'nosotros', component: FeatureComponent, data: { titulo: 'Nosotros' } },
+
+  { path: 'programas', component: CoursesComponent, data: { titulo: 'Programas' } },
   // { path: 'financiacion', component: FinanciacionComponent, data: { titulo: 'Financiación' } },
-  // { path: 'admisiones', component: AdmisionesComponent, data: { titulo: 'Admisiones' } },
-  // { path: 'contact', component: ContactComponent, data: { titulo: 'Contact' } },
+  { path: 'admisiones', component: ReviewComponent, data: { titulo: 'Admisiones' } },
+  { path: 'contact', component: AddressComponent, data: { titulo: 'Contact' } },
 ]
 
 @NgModule({
